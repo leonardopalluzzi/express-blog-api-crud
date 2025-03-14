1874eaaed9d36b9d0c4f5511fa68842c16044935
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-14 14:32:56 +0100

    add functions to controller file

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,1 +1,48 @@
 const data = require('../data/posts');
+
+function index(req, res) {
+    res.json(data);
+}
+
+function show(req, res) {
+    const post = data.find(post => post.slug == req.params.slug);
+
+    //for loop implementation
+    /*
+    let post;
+    for(let i = 0; i < data.length; i++){
+        const reqPost = req.params.slug
+        if(data[i].slug == reqPost){
+            post = data[i];
+            break;
+        }
+    }
+    */
+
+    res.json(post);
+}
+
+function store(req, res) {
+    res.send('Upload a new post');
+}
+
+function update(req, res) {
+    res.send(`Update the post: ${req.params.slug}`);
+}
+
+function modify(req, res) {
+    res.send(`Modify the post: ${req.params.slug}`);
+}
+
+function destroy(req, res) {
+    res.send(`Delete the post: ${req.params.slug}`);
+}
+
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy
+}
--- routers/posts.js
+++ routers/posts.js
@@ -7,48 +7,32 @@ router.use(express.static('public'));
 
 //index
 router.get('/', (req, res) => {
-    //res.send('Posts list');
-    res.json(data);
+    
 });
 
 //show
 router.get('/:slug', (req, res) => {
-    //res.send(`Showing post: ${req.params.slug}`);
-    const post = data.find(post => post.slug == req.params.slug);
-
-    //for loop implementation
-    /*
-    let post;
-    for(let i = 0; i < data.length; i++){
-        const reqPost = req.params.slug
-        if(data[i].slug == reqPost){
-            post = data[i];
-            break;
-        }
-    }
-    */
-
-    res.json(post);
+    
 });
 
 //store
 router.post('/', (req, res) => {
-    res.send('Upload a new post');
+    
 });
 
 //update
 router.put('/:slug', (req, res) => {
-    res.send(`Update the post: ${req.params.slug}`);
+    
 });
 
 //modify
 router.patch('/:slug', (req, res) => {
-    res.send(`Modify the post: ${req.params.slug}`);
+    
 });
 
 //delete
 router.delete('/:slug', (req, res) => {
-    res.send(`Delete the post: ${req.params.slug}`);
+    
 });
 
 module.exports = router;
